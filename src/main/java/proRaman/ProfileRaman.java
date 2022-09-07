e4ff71865531bf698448b776b52c502193f87a84
ramancset <raman27cset@gmail.com>
2022-09-07 15:49:36 +0530

    raman

---
+++ src/main/java/proRaman/ProfileRaman.java
@@ -0,0 +1,12 @@
+package proRaman;
+
+public class ProfileRaman {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("raman");
+		
+
+	}
+
+}
---
+++ src/test/java/FB/stClass.java
@@ -0,0 +1,15 @@
+package FB;
+
+public class stClass {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		String s= "Good Morning";
+       StringBuffer s1 = new StringBuffer(s);
+       System.out.print(s1.reverse());
+        
+
+	}
+
+}
